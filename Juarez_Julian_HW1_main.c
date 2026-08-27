3499422a8f7d02efdfeed166bbc07e9b471a61b1
JulianJua <juarezjulian078@gmail.com>
2026-08-27 12:02:46 -0700

    printf

---
+++ Juarez_Julian_HW1_main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+* Class::  CSC-415-01 Fall 2026
+* Name:: Julian Juarez
+* Student ID:: 923162354
+* GitHub-Name:: JulianJua
+* Project:: Assignment 1 – Command Line Arguments
+*
+* File:: <Juarez_Julian_HW1_main.c>
+*
+* Description::
+*
+**************************************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("There were %d arguments on the command line.\n", argc); 
