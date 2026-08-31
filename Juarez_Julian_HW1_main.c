347ac7865df5a924c1122660b0f87ba36e6e1ba7
JulianJua <juarezjulian078@gmail.com>
2026-08-30 17:19:00 -0700

    header file, for loop, comments.

--- Juarez_Julian_HW1_main.c
+++ Juarez_Julian_HW1_main.c
@@ -7,16 +7,17 @@
 *
 * File:: <Juarez_Julian_HW1_main.c>
 *
-* Description:: 
+* Description:: Program prints the count of command-line arguments and its corresponding arguments
+* Count and arguments are subsequently newlined for format. 
 *
 **************************************************************/
-#include <stdio.h>
+#include <stdio.h> 
 
-int main(int args, char *argv[]) {
-	printf("There were %d arguments on the command line.\n", args);
+int main(int args, char *argv[]) { // array of pointers, C has no string type, so we use char pointers to represent strings
+	printf("There were %d arguments on the command line.\n", args); 
 	
-	for (int i = 0; i < args; i++) {
-		printf("Argument %d: \t%s\n", i, argv[i]);
+	for (int i = 0; i < args; i++) { // loop from 0 to args - 1, staying within bounds of the array
+		printf("Argument %02d: \t%s\n", i, argv[i]); // %02d for format, 0 for padding, 2 for width
 	}
 	return 0;
 }
